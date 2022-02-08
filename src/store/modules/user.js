fb61749097c6971c5f1ec8b18467f934ca127001
auoHan <710543268@qq.com>
2022-02-08 23:10:51 +0800

    改造store的user

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -32,7 +32,6 @@ const actions = {
   async login({ commit }, userInfo) {
     const { username, password } = userInfo
     const result = await login({ username: username.trim(), password: password })
-    console.log(result)
     if (result.code === 20000) {
       commit('SET_TOKEN', result.data.token)
       setToken(result.data.token)
@@ -43,24 +42,21 @@ const actions = {
   },
 
   // get user info
-  getInfo({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
+  async getInfo({ commit, state }) {
+    const result = await getInfo(state.token)
+    if (result.code === 20000) {
+      const { data } = result
+      if (!data) {
+        return Promise.reject('Verification failed, please Login again.')
+      }
+      const { name, avatar } = data
 
-        if (!data) {
-          return reject('Verification failed, please Login again.')
-        }
-
-        const { name, avatar } = data
-
-        commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
-    })
+      commit('SET_NAME', name)
+      commit('SET_AVATAR', avatar)
+      return Promise.resolve(data)
+    } else {
+      return Promise.reject(new Error())
+    }
   }
   ,
 
