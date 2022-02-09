815752db24da8e1b2311081b6b371ceddf0f99f2
auoHan <710543268@qq.com>
2022-02-09 18:12:53 +0800

    收集属性名

--- src/views/product/Attr/index.vue
+++ src/views/product/Attr/index.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
     <el-card style="margin: 20px 0">
-      <CategorySelect @getCategoryId="getCategoryId"/>
+      <CategorySelect @getCategoryId="getCategoryId" />
     </el-card>
     <el-card>
       <div v-show="isShowTable">
-        <el-button type="primary" icon="el-icon-plus" style="margin-bottom: 20px" :disabled="!category3Id" @click="isShowTable=false">添加属性</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          style="margin-bottom: 20px"
+          :disabled="!category3Id"
+          @click="isShowTable=false"
+        >添加属性
+        </el-button>
         <el-table style="width: 100%" border :data="attrList">
-          <el-table-column type="index" label="序号" width="80" align="center"/>
-          <el-table-column prop="attrName" label="属性名称" width="150"/>
+          <el-table-column type="index" label="序号" width="80" align="center" />
+          <el-table-column prop="attrName" label="属性名称" width="150" />
           <el-table-column prop="prop" label="属性值列表" width="width">
             <template v-slot="{row}">
               <el-tag v-for="attrValue in row.attrValueList" :key="attrValue.id" type="success" style="margin: 0 10px">
@@ -18,40 +25,39 @@
           </el-table-column>
           <el-table-column prop="prop" label="操作" width="150">
             <template v-slot="{row}">
-              <el-button type="warning" icon="el-icon-edit" size="mini" @click="isShowTable=false"/>
-              <el-button type="danger" icon="el-icon-delete" size="mini"/>
+              <el-button type="warning" icon="el-icon-edit" size="mini" @click="isShowTable=false" />
+              <el-button type="danger" icon="el-icon-delete" size="mini" />
             </template>
           </el-table-column>
         </el-table>
       </div>
 
       <div v-show="!isShowTable">
-        <el-form :inline="true" label-width="80px" >
-          <el-form-item label="属性名">
+        <el-form ref="attrInfo" :inline="true" label-width="80px" :model="attrInfo" :rules="rules">
+          <el-form-item label="属性名" prop="attrName">
             <el-input
+              v-model="attrInfo.attrName"
               placeholder="请输入属性名"
-            ></el-input>
+            />
           </el-form-item>
         </el-form>
         <el-button
           type="primary"
           icon="el-icon-plus"
-        >添加属性值</el-button
-        >
+        >添加属性值
+        </el-button>
         <el-button @click="isShowTable = true">取消</el-button>
         <el-table
-          style="width: 100%; margin: 20px 0px"
+          style="width: 100%; margin: 20px 0"
           border
         >
-          <el-table-column align="center" type="index" label="序号" width="80">
-          </el-table-column>
+          <el-table-column align="center" type="index" label="序号" width="80" />
           <el-table-column width="width" prop="prop" label="属性值名称">
             <template v-slot="{ row }">
               <!-- 这里结构需要用到span与input进行来回的切换 -->
             </template>
           </el-table-column>
-          <el-table-column width="width" prop="prop" label="操作">
-          </el-table-column>
+          <el-table-column width="width" prop="prop" label="操作" />
         </el-table>
         <el-button type="primary">保存</el-button>
         <el-button @click="isShowTable = true">取消</el-button>
@@ -71,7 +77,24 @@ export default {
       category2Id: '',
       category3Id: '',
       attrList: [],
-      isShowTable: false
+      isShowTable: false,
+      attrInfo: {
+        attrName: '',
+        attrValueList: [
+          {
+            attrId: 0,
+            valueName: ''
+          }
+        ],
+        categoryId: 0,
+        categoryLevel: 3
+      },
+      rules: {
+        attrName: [
+          { required: true, message: '请输入属性名', trigger: 'blur' },
+          { min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'change' }
+        ]
+      }
     }
   },
   methods: {
