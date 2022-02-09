15c7022bfc4f6772c42e7732992b4d200c74635b
auoHan <710543268@qq.com>
2022-02-10 01:25:27 +0800

    根据isShowTable决定三级联动选择是否可选

--- src/components/CategorySelect/index.vue
+++ src/components/CategorySelect/index.vue
@@ -2,17 +2,17 @@
   <div>
     <el-form :inline="true" class="demo-form-inline" :model="cForm">
       <el-form-item label="一级分类">
-        <el-select v-model="cForm.category1Id" placeholder="请选择" @change="handler1">
+        <el-select v-model="cForm.category1Id" placeholder="请选择" :disabled="show" @change="handler1">
           <el-option v-for="c1 in list1" :key="c1.id" :label="c1.name" :value="c1.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="二级分类">
-        <el-select v-model="cForm.category2Id" placeholder="请选择" @change="handler2">
+        <el-select v-model="cForm.category2Id" placeholder="请选择" :disabled="show" @change="handler2">
           <el-option v-for="c2 in list2" :key="c2.id" :label="c2.name" :value="c2.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="三级分类">
-        <el-select v-model="cForm.category3Id" placeholder="请选择" @change="handler3">
+        <el-select v-model="cForm.category3Id" placeholder="请选择" :disabled="show" @change="handler3">
           <el-option v-for="c3 in list3" :key="c3.id" :label="c3.name" :value="c3.id"></el-option>
         </el-select>
       </el-form-item>
@@ -25,6 +25,7 @@ import { reqCategory1List, reqCategory2List, reqCategory3List } from '@/api/prod
 
 export default {
   name: 'CategorySelect',
+  props: ['show'],
   data() {
     return {
       list1: [],
