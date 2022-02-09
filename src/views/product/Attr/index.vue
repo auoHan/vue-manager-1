7e4411207a92f7036984b1db7c4b04bdf3891fc8
auoHan <710543268@qq.com>
2022-02-09 17:43:41 +0800

    添加属性的静态展示

--- src/views/product/Attr/index.vue
+++ src/views/product/Attr/index.vue
@@ -1,26 +1,61 @@
 <template>
   <div>
     <el-card style="margin: 20px 0">
-      <CategorySelect @getCategoryId="getCategoryId"></CategorySelect>
+      <CategorySelect @getCategoryId="getCategoryId"/>
     </el-card>
     <el-card>
-      <el-button type="primary" icon="el-icon-plus" style="margin-bottom: 20px">添加属性</el-button>
-      <el-table style="width: 100%" border :data="attrList">
-        <el-table-column type="index" label="序号" width="80" align="center">
-        </el-table-column>
-        <el-table-column prop="attrName" label="属性名称" width="150"></el-table-column>
-        <el-table-column prop="prop" label="属性值列表" width="width">
-          <template v-slot="{row}">
-            <el-tag type="success" v-for="attrValue in row.attrValueList" :key="attrValue.id" style="margin: 0 10px">{{ attrValue.valueName }}</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="prop" label="操作" width="150">
-          <template v-slot="{row}">
-            <el-button type="warning" icon="el-icon-edit" size="mini"></el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+      <div v-show="isShowTable">
+        <el-button type="primary" icon="el-icon-plus" style="margin-bottom: 20px" :disabled="!category3Id" @click="isShowTable=false">添加属性</el-button>
+        <el-table style="width: 100%" border :data="attrList">
+          <el-table-column type="index" label="序号" width="80" align="center"/>
+          <el-table-column prop="attrName" label="属性名称" width="150"/>
+          <el-table-column prop="prop" label="属性值列表" width="width">
+            <template v-slot="{row}">
+              <el-tag v-for="attrValue in row.attrValueList" :key="attrValue.id" type="success" style="margin: 0 10px">
+                {{ attrValue.valueName }}
+              </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column prop="prop" label="操作" width="150">
+            <template v-slot="{row}">
+              <el-button type="warning" icon="el-icon-edit" size="mini" @click="isShowTable=false"/>
+              <el-button type="danger" icon="el-icon-delete" size="mini"/>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+
+      <div v-show="!isShowTable">
+        <el-form :inline="true" label-width="80px" >
+          <el-form-item label="属性名">
+            <el-input
+              placeholder="请输入属性名"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+        >添加属性值</el-button
+        >
+        <el-button @click="isShowTable = true">取消</el-button>
+        <el-table
+          style="width: 100%; margin: 20px 0px"
+          border
+        >
+          <el-table-column align="center" type="index" label="序号" width="80">
+          </el-table-column>
+          <el-table-column width="width" prop="prop" label="属性值名称">
+            <template v-slot="{ row }">
+              <!-- 这里结构需要用到span与input进行来回的切换 -->
+            </template>
+          </el-table-column>
+          <el-table-column width="width" prop="prop" label="操作">
+          </el-table-column>
+        </el-table>
+        <el-button type="primary">保存</el-button>
+        <el-button @click="isShowTable = true">取消</el-button>
+      </div>
     </el-card>
   </div>
 </template>
@@ -35,7 +70,8 @@ export default {
       category1Id: '',
       category2Id: '',
       category3Id: '',
-      attrList: []
+      attrList: [],
+      isShowTable: false
     }
   },
   methods: {
