95903a8980c43c5d4742186e78f1b36e87d23501
auoHan <710543268@qq.com>
2022-02-09 16:29:06 +0800

    三级联动获取数据完成

--- src/views/product/Attr/index.vue
+++ src/views/product/Attr/index.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>Attr</div>
+  <div>
+    <el-card style="margin: 20px 0">
+      <CategorySelect @getCategoryId="getCategoryId"></CategorySelect>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Attr'
+  name: 'Attr',
+  data() {
+    return {
+      category1Id: '',
+      category2Id: '',
+      category3Id: ''
+    }
+  },
+  methods: {
+    getCategoryId({ categoryId, level }) {
+      if (level === 1) {
+        this.category1Id = categoryId
+        this.category2Id = ''
+        this.category3Id = ''
+      } else if (level === 2) {
+        this.category2Id = categoryId
+        this.category3Id = ''
+      } else if (level === 3) {
+        this.category3Id = categoryId
+        this.getAttrList()
+      }
+    },
+    getAttrList() {
+      console.log('发请求')
+    }
+  }
 }
 </script>
 
