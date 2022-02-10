0f92f887efe0118fb1704ee3869780065157a6b1
auoHan <710543268@qq.com>
2022-02-10 20:50:43 +0800

    获取spu图片

--- src/api/product/spu.js
+++ src/api/product/spu.js
@@ -7,3 +7,4 @@ export const reqSpuList = (page, limit, category3Id) => request({
 })
 export const reqSpu = (spuId) => request({ url: `/admin/product/getSpuById/${spuId}`, method: 'get' })
 export const reqTradeMarkList = () => request({ url: '/admin/product/baseTrademark/getTrademarkList', method: 'get' })
+export const reqSpuImageList = (spuId) => request({ url: `/admin/product/spuImageList/${spuId}`, method: 'get' })
