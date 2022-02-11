1486280aa495244afc119c70e6527e5415b6249b
auoHan <710543268@qq.com>
2022-02-11 14:52:55 +0800

    获取图片的数据、获取销售属性的数据、获取平台属性的数据的API添加

--- src/api/product/spu.js
+++ src/api/product/spu.js
@@ -17,3 +17,8 @@ export const reqAddOrUpdateSpu = (spuInfo) => {
   }
 }
 export const reqDeleteSpu = (spuId) => request({ url: `/admin/product/deleteSpu/${spuId}`, method: 'delete' })
+export const reqSpuSaleAttrList = (spuId) => request({ url: `/admin/product/spuSaleAttrList/${spuId}`, method: 'get' })
+export const reqAttrInfoList = (category1Id, category2Id, category3Id) => request({
+  url: `/admin/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`,
+  method: 'get'
+})
