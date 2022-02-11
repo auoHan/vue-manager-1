486109bcc627d3dc3833037f49618497408bd2b8
auoHan <710543268@qq.com>
2022-02-11 16:49:52 +0800

    展示SkuForm的销售属性和平台属性数据，收集除图片以外的sku数据

--- src/views/product/Spu/SkuForm/index.vue
+++ src/views/product/Spu/SkuForm/index.vue
@@ -1,33 +1,33 @@
 <template>
   <div>
     <el-form ref="form" label-width="80px">
-      <el-form-item label="SPU名称">qqq</el-form-item>
+      <el-form-item label="SPU名称">{{ spu.spuName }}</el-form-item>
       <el-form-item label="SKU名称">
-        <el-input placeholder="sku名称"/>
+        <el-input v-model="skuInfo.skuName" placeholder="sku名称" />
       </el-form-item>
       <el-form-item label="价格(元)">
-        <el-input placeholder="价格(元)" type="number"/>
+        <el-input-number v-model="skuInfo.price" :precision="2" placeholder="价格(元)" controls-position="right" :min="1" :max="99999" />
       </el-form-item>
       <el-form-item label="重量(千克)">
-        <el-input placeholder="重量(千克)"/>
+        <el-input-number v-model="skuInfo.weight" :precision="2" placeholder="重量(千克)" controls-position="right" :min="1" :max="9999" />
       </el-form-item>
       <el-form-item label="规格描述">
-        <el-input placeholder="规格描述" type="textarea" rows="4"/>
+        <el-input v-model="skuInfo.skuDesc" placeholder="规格描述" type="textarea" rows="4" />
       </el-form-item>
       <el-form-item label="平台属性">
         <el-form ref="form" :inline="true" label-width="80px">
-          <el-form-item label="label">
-            <el-select placeholder="请选择" value="">
-              <el-option label="label" value=""/>
+          <el-form-item v-for="attr in attrInfoList" :key="attr.id" :label="attr.attrName">
+            <el-select v-model="attr.attrIdAndAttrValueId" placeholder="请选择">
+              <el-option v-for="attrValue in attr.attrValueList" :key="attrValue.id" :label="attrValue.valueName" :value="`${attr.id}:${attrValue.id}`" />
             </el-select>
           </el-form-item>
         </el-form>
       </el-form-item>
       <el-form-item label="销售属性">
         <el-form ref="form" :inline="true" label-width="80px">
-          <el-form-item label="label">
-            <el-select placeholder="请选择" value="">
-              <el-option label="label" value=""/>
+          <el-form-item v-for="saleAttr in spuSaleAttrList" :key="saleAttr.id" :label="saleAttr.saleAttrName">
+            <el-select v-model="saleAttr.saleAttrIdAndSaleAttrValueId" placeholder="请选择">
+              <el-option v-for="saleAttrValue in saleAttr.spuSaleAttrValueList" :key="saleAttrValue.id" :label="saleAttrValue.saleAttrValueName" :value="`${saleAttr.id}:${saleAttrValue.id}`" />
             </el-select>
           </el-form-item>
         </el-form>
@@ -35,13 +35,13 @@
       <el-form-item label="图片列表">
         <!--@selection-change="handleSelectionChange"-->
         <el-table style="width: 100%" border>
-          <el-table-column type="selection" align="center" prop="prop" width="80"/>
+          <el-table-column type="selection" align="center" prop="prop" width="80" />
           <el-table-column prop="prop" label="图片" width="width">
             <template slot-scope="{row,$index}">
               <img :src="row.imgUrl" style="width:100px;height:100px">
             </template>
           </el-table-column>
-          <el-table-column prop="imgName" label="名称" width="width"/>
+          <el-table-column prop="imgName" label="名称" width="width" />
           <el-table-column prop="prop" label="操作" width="width">
             <template slot-scope="{row,$index}">
               <el-button v-if="row.isDefault===0" type="primary">设置默认</el-button>
@@ -67,11 +67,64 @@ export default {
     return {
       spuImageList: [],
       spuSaleAttrList: [],
-      attrInfoList: []
+      attrInfoList: [],
+      // 收集sku数据的字段
+      skuInfo: {
+        // 第一类收集的数据：父组件给的数据
+        category3Id: 0,
+        spuId: 0,
+        tmId: 0,
+        // 第二类：需要通过数据双向绑定v-model收集
+        skuName: '',
+        price: 0,
+        weight: '',
+        skuDesc: '',
+        // 第三类：需要自己书写代码
+        // 默认图片
+        skuDefaultImg: '',
+        // 收集图片的字段
+        skuImageList: [
+          // {
+          //   id: 0,
+          //   imgName: "string",
+          //   imgUrl: "string",
+          //   isDefault: "string",
+          //   skuId: 0,
+          //   spuImgId: 0,
+          // },
+        ],
+        // 平台属性
+        skuAttrValueList: [
+          // {
+          //   attrId: 0,
+          //   valueId: 0,
+          // },
+        ],
+        // 销售属性
+        skuSaleAttrValueList: [
+          // {
+          //   id: 0,
+          //   saleAttrId: 0,
+          //   saleAttrName: "string",
+          //   saleAttrValueId: 0,
+          //   saleAttrValueName: "string",
+          //   skuId: 0,
+          //   spuId: 0,
+          // },
+        ]
+      },
+      spu: {},
+      // 收集图片的数据字段:但是需要注意，收集的数据目前缺少isDefault字段，将来提交给服务器数据的时候，需要整理参数
+      imageList: []
     }
   },
   methods: {
     async getData(category1Id, category2Id, spu) {
+      this.spu = spu
+      // 收集父组件给予的数据
+      this.skuInfo.category3Id = spu.category3Id
+      this.skuInfo.spuId = spu.id
+      this.skuInfo.tmId = spu.tmId
       const spuImageResult = await reqSpuImageList(spu.id)
       if (spuImageResult.code === 200) {
         this.spuImageList = spuImageResult.data
