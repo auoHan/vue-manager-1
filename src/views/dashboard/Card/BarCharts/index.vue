03535cfad284ffdcd96f7c2c1e1be7b61b5e76d3
auoHan <710543268@qq.com>
2022-02-12 23:23:23 +0800

    柱状图完成

--- src/views/dashboard/Card/BarCharts/index.vue
+++ src/views/dashboard/Card/BarCharts/index.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>BarCharts</div>
+  <div ref="charts" class="charts" />
 </template>
 
 <script>
+import * as echarts from 'echarts'
+
 export default {
-  name: 'BarCharts'
+  name: 'BarCharts',
+  mounted() {
+    const barCharts = echarts.init(this.$refs.charts)
+    barCharts.setOption({
+      xAxis: {
+        // 隐藏x轴
+        show: false,
+        // 均分
+        type: 'category'
+      },
+      yAxis: {
+        // 隐藏y轴
+        show: false
+      },
+      // 系列
+      series: [
+        {
+          // 图标形式-柱状图
+          type: 'bar',
+          data: [10, 7, 33, 12, 48, 9, 29, 10, 44, 33, 22, 8],
+          color: '#428675'
+        }
+      ],
+      // 布局调试
+      grid: {
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0
+      }
+    })
+  }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.charts {
+  height: 100%;
+  width: 100%;
+}
 </style>
