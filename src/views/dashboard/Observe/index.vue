25dc9f8e4313dd84d17dcfcf9be1ee07e1d21728
auoHan <710543268@qq.com>
2022-02-13 03:38:37 +0800

    创建Observe组件，编写静态架构，引入Search组件和Category组件

---
+++ src/views/dashboard/Observe/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-row :gutter="10">
+      <el-col :span="12">
+        <Search></Search>
+      </el-col>
+      <el-col :span="12">
+        <Category></Category>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Search from '@/views/dashboard/Observe/Search'
+import Category from '@/views/dashboard/Observe/Category'
+export default {
+  name: 'Observe',
+  components: { Category, Search }
+}
+</script>
+
+<style scoped>
+
+</style>
