2a27418fa6196b48cab7692558cf680604af7a7a
auoHan <710543268@qq.com>
2022-02-13 03:36:09 +0800

    引入Sale和Observe组件

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <Card></Card>
+    <Sale></Sale>
+    <Observe></Observe>
   </div>
 </template>
 
 <script>
 
 import Card from '@/views/dashboard/Card'
+import Sale from '@/views/dashboard/Sale'
+import Observe from '@/views/dashboard/Observe'
 export default {
   name: 'Dashboard',
-  components: { Card }
+  components: { Observe, Sale, Card }
 }
 </script>
 
